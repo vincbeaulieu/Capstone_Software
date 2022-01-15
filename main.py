dfa2d59e46f2ddca452bbffbeb1e79c7e1e50059
Mark Zalass <markzalass@gmail.com>
2022-01-15 17:32:13 -0500

    Placeholder noise generator, requirements.txt, fix input_data, output_data not found in machine learning

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-
 import MachineLearning as ml
+import noise_generator
 
 def main():
     test()
-
     return 0
 
 def test():
     # Comment the test when done
     ml.test()
+    noise_generator.test() # placeholder
 
     pass
 
