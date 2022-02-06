0802a956288ba64fa0fb12b094bef5e848ef9fea
Hamza Shah <hamzasyedshah2018@outlook.com>
2022-02-06 18:46:27 -0500

    Fix format

--- servoGestureOutput.py
+++ servoGestureOutput.py
@@ -4,29 +4,33 @@
 channel 1."""
 import time
 from adafruit_servokit import ServoKit
+
 # Set channels to the number of servo channels on your kit.
 # 16 for FeatherWing, 16 for Shield/HAT/Bonnet.
 kit = ServoKit(channels=16)
 
 gesture = {
-    'close':[-1,-1,1,1,1], # close hand
-    'open':[1,1,-1,-1,-1], # open hand
-    'thumbs':[1,-1,1,1,1], # thumbs up
-    'flip':[-1,-1,-1,1,1], # flip off
-    'rock':[-1,1,1,1,-1], # rock and roll
-    'peace':[-1,1,-1,1,1], # peace
-    'ok':[-1,-1,-1,-1,-1], # ok
-    'exit':[1,1,-1,-1,-1] # exit
+    'handClose': [-1, -1, 1, 1, 1],  # close hand
+    'handOpen': [1, 1, -1, -1, -1],  # open hand
+    'handThumbsUp': [1, -1, 1, 1, 1],  # thumbs up
+    'handFlip': [-1, -1, -1, 1, 1],  # flip off
+    'handRock': [-1, 1, 1, 1, -1],  # rock and roll
+    'handPeace': [-1, 1, -1, 1, 1],  # peace
+    'handOk': [-1, -1, -1, -1, -1],  # ok
+    'handExit': [1, 1, -1, -1, -1]  # exit
 }
 
+
 def motion(input):
-    for servo in range(0,5):
-        kit.continuous_servo[servo].throttle = gesture[input][servo] 
+    for servo in range(0, 5):
+        kit.continuous_servo[servo].throttle = gesture[input][servo]
+
 
 def validGestures():
     print("\nValid Gestures:")
     for key in gesture.keys():
         print("    " + key)
+
 
 validGestures()
 
