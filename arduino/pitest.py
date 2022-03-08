7643ccb25c0fa1eac997c4c0beb4340d763c894c
Mark Zalass <markzalass@gmail.com>
2022-03-07 19:34:10 -0500

    Serial interface between arduino and pi

--- arduino/pitest.py
+++ arduino/pitest.py
@@ -1,11 +1,15 @@
-#!/usr/bin/env python3
 import serial
+from time import sleep
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-    ser.reset_input_buffer()
-
-    while True:
-        if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip()
-            print(line)
+    with serial.Serial("/dev/ttyUSB0", 9600, timeout=1) as arduino:
+        sleep(0.1)
+        if arduino.isOpen():
+            print("{} connected!".format(arduino.port))
+            count = 0
+            while count < 10:
+                arduino.write(b'\x01')
+                count += 1
+                print(count)
+                sleep(1)
+            arduino.write(b'\x00')
