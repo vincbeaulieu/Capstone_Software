12f950777618a74577029a67d7f199bffd6f651e
Vincent B <37360448+vincbeaulieu@users.noreply.github.com>
2022-03-10 20:07:56 -0500

    Create encoder.py

---
+++ ml_training/encoder.py
@@ -0,0 +1,9 @@
+
+
+# One Hot Encoder
+def one_hot_encoder(sequence,categories,scale=None,remove_last=True):
+    scale = (scale,len(categories))[scale==None]
+    mapping = dict(zip(categories, range(scale)))
+    results = [mapping[i] for i in sequence]
+    output = np.rot90(np.eye(scale, dtype=int)[results])
+    return (output,output[:-1])[remove_last]
