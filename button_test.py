47116decff768c9802cdd97fe902f0c7f1e5b784
Hamza Shah <hamzasyedshah2018@outlook.com>
2022-03-12 13:34:50 -0500

    UPdate 5.0

--- button_test.py
+++ button_test.py
@@ -1,34 +1,30 @@
 # Sample code from https://raspberrypihq.com/use-a-push-button-with-raspberry-pi-gpio/
-import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
-import time
+from time import sleep, time
+import RPi.GPIO as GPIO
 
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setwarnings(False)  # Ignore warning for now
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Set pin 10 to be an input pin
 
-def myInterrupt(channel):
+while True:  # Run forever
     global buttonStatus
     start_time = time.time()
-
-    while GPIO.input(channel) == 0:  # Wait for the button up
-        pass
 
     buttonTime = time.time() - start_time  # How long was the button down?
 
     if buttonTime >= 4:
         buttonStatus = 3
         print("Button was pushed for a very long time!")
+        time.sleep(0.5)
+
     elif buttonTime >= 2:
         buttonStatus = 2
         print("Button was pushed for a long time!")
+        time.sleep(0.5)
+        
     elif buttonTime >= .1:
         buttonStatus = 1
         print("Button was pushed!")
+        time.sleep(0.5)
 
-
-GPIO.setwarnings(False)  # Ignore warning for now
-GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set pin 10 to be an input pin
-GPIO.add_event_detect(10, GPIO.FALLING, callback=myInterrupt, bouncetime=500)
-
-while True:  # Run forever
-    if GPIO.input(10) == GPIO.HIGH:
-        print("Button was pushed!")
-        time.sleep(0.1)
+GPIO.cleanup()
