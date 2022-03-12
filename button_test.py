74ae56d82bc2f2bd79c4b27b33fba6570cc0e643
mzal98 <markzalass@gmail.com>
2022-03-12 19:39:08 +0000

    Update 7.0

--- button_test.py
+++ button_test.py
@@ -4,7 +4,7 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set pin 10 to be an input pin
-GPIO.setwarnings(False) # Ignore warning for now
+#GPIO.setwarnings(False) # Ignore warning for now
 #
 # def myInterrupt(channel):
 #     global buttonStatus
@@ -25,9 +25,33 @@ GPIO.setwarnings(False) # Ignore warning for now
 #
 # GPIO.add_event_detect(10, GPIO.FALLING, callback=myInterrupt, bouncetime=500)
 
-timer = time()
+#timer = time()
 
-while True: # Run forever
-    if GPIO.input(10) == GPIO.HIGH:
-        print("Button was pushed!")
-        sleep(0.5)
+#while True: # Run forever
+#   if GPIO.input(10) == GPIO.HIGH:
+#       print(timer)
+#       print("Button was pushed!")
+#        sleep(0.5)
+
+def my_callback(channel):
+    global start
+    global end
+    if GPIO.input(10) == 1:
+        start = time()
+    if GPIO.input(10) == 0:
+        end = time()
+        elapsed = end - start
+        print(elapsed)
+
+    if elapsed >= 4:
+        buttonStatus = 3
+    elif elapsed >= 2:
+        buttonStatus = 2  
+   elif elapsed >= .1: 
+        buttonStatus = 1
+
+GPIO.add_event_detect(10, GPIO.BOTH, callback=my_callback, bouncetime=200)
+
+while True:
+    print("-")
+    sleep(0.25)
