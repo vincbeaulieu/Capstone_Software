f6fc3e29bb8d4ea462b40cd7bd08012c64151b45
Hamza Shah <hamzasyedshah2018@outlook.com>
2022-03-12 16:34:54 -0500

    Test

--- button_test.py
+++ button_test.py
@@ -4,51 +4,29 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set pin 10 to be an input pin
-#GPIO.setwarnings(False) # Ignore warning for now
-#
-# def myInterrupt(channel):
-#     global buttonStatus
-#     start_time = time()
-#
-#     while GPIO.input(channel) == 0:  # Wait for the button up
-#         pass
-#
-#     buttonTime = time() - start_time  # How long was the button down?
-#
-#     if buttonTime >= 4:
-#         buttonStatus = 3
-#     elif buttonTime >= 2:
-#         buttonStatus = 2
-#     elif buttonTime >= .1:
-#         buttonStatus = 1
-#
-#
-# GPIO.add_event_detect(10, GPIO.FALLING, callback=myInterrupt, bouncetime=500)
 
-#timer = time()
-
-#while True: # Run forever
-#   if GPIO.input(10) == GPIO.HIGH:
-#       print(timer)
-#       print("Button was pushed!")
-#        sleep(0.5)
 
 def my_callback(channel):
     global start
     global end
+    global buttonStatus
+
     if GPIO.input(10) == 1:
         start = time()
     if GPIO.input(10) == 0:
         end = time()
         elapsed = end - start
-        print(elapsed)
 
     if elapsed >= 4:
         buttonStatus = 3
+        print("Button was pushed for a while!")
     elif elapsed >= 2:
-        buttonStatus = 2  
-   elif elapsed >= .1: 
+        buttonStatus = 2
+        print("Button was pushed for a bit!")
+    elif elapsed >= .1:
         buttonStatus = 1
+        print("Button was pushed!")
+
 
 GPIO.add_event_detect(10, GPIO.BOTH, callback=my_callback, bouncetime=200)
 
