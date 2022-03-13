a2e7589484d91d79a0ad9b1e2c4479591f40c1ce
Vincent B <37360448+vincbeaulieu@users.noreply.github.com>
2022-03-12 22:21:51 -0500

    Refractor

--- servoGestureOutput.py
+++ servoGestureOutput.py
@@ -17,9 +17,9 @@ gesture = {
     'handRock':     [ 1, -1,  1, -1, -1],  # rock and roll
     'handPeace':    [ 1, -1, -1, -1,  1],  # peace
     'handOk':       [ 1,  1, -1,  1, -1],  # ok
-    'handIndex':    [1,-1, 1, -1, 1],
-    'handRing':     [1, 1, 1, 1, 1],
-    'handPinky':    [1, 1, 1, -1, -1],
+    'handIndex':    [ 1, -1,  1, -1,  1],
+    'handRing':     [ 1,  1,  1,  1,  1],
+    'handPinky':    [ 1,  1,  1, -1, -1],
     'handExit':     [-1, -1, -1,  1, -1]  # exit
 }
 
