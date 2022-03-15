538770f7c47e0b7968ddc52db64233bbb87b04a4
Vincent B <37360448+vincbeaulieu@users.noreply.github.com>
2022-03-15 09:06:19 -0400

    ML approach Done

--- ml/lda.py
+++ ml/lda.py
@@ -1,40 +1,7 @@
 
-from myoband.MyoBandData import read_myoband_data, get_myoband_data
-from ml_class import train_model, get_prediction
-from rbpi.servoGestureOutput import motion
-import multiprocessing
-from time import sleep
+from ml_class import train_model, myo_predict
 
-def myo_predict(ml_model, ml_scaler):
-    # Main Code
-    q1 = multiprocessing.Queue()
-    q2 = multiprocessing.Queue()
-    p = multiprocessing.Process(target=read_myoband_data, args=(q1, q2,))
-    try:
-        print("Starting myoband...")
-        p.start()
-        sleep(5)
-
-        # Wait for user input
-        input("Press enter to start")
-
-        while True:
-
-            # Read data from EMG sensors
-            emg1, emg2 = get_myoband_data(q1, q2)
-            emg_data = [emg1 + emg2]
-
-            # Use the ML model on the EMG data
-            prediction = get_prediction(emg_data, ml_model, ml_scaler)
-
-            # Display and Perform the predicted gesture
-            print(prediction)
-            motion(prediction)
-
-    except KeyboardInterrupt:
-        p.terminate()
-        p.join()
-
+# LDA Code:
 if __name__ == "__main__":
 
     # Import and create a ML model
@@ -44,6 +11,9 @@ if __name__ == "__main__":
     # Train the ML model
     dataset_name = "suyash10gpieday.csv"
     lda_model, lda_scaler = train_model(lda_model, dataset_name)
+
+    # Use the model on the emg data
+    myo_predict(lda_model, lda_scaler)
 
 
 
