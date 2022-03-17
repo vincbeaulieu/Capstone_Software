f0602d020c886956b0d9d0ab9c752f3bc0225dd6
Vincent B <37360448+vincbeaulieu@users.noreply.github.com>
2022-03-17 04:13:19 -0400

    Dual ML Done
    ml_class.py is improved
    evaluation method was added
    
    lda.py is now more of a general testing hub of many ml's
    
    dual_ml is completed - may require some refactoring
    benchmark implemented in dual ml
    
    BaggingClassifier combined with HistGradientBoostingClassifier yield the best accuracy (80%+) but way too slow for us to implement (500ms +)
    
    HistGradientBoostingClassifier() alone yield 79%, and when combined in dual ML, output accuracy is 77%, with 50ms on a 2.3GHz cpu (90ms on 1.2GHz - rbpi cpu)

--- myoband/MyoBandData.py
+++ myoband/MyoBandData.py
@@ -34,56 +34,62 @@ def get_myoband_data(q1, q2):
 
 def read_myoband_data(q1, q2):
 
-    # To change the mode of data, edit mode=emg_mode.<YourMode>
-    myo_1 = Myo(mode=emg_mode.RAW)
-    myo_2 = Myo(mode=emg_mode.RAW)
-
-    myo_1.connect(ADDRESS_MYO1)  # RED LED
-    myo_2.connect(ADDRESS_MYO2)  # Turquoise LED
-
-    # Todo: check what the output print when connected, and check the output when disconnected
-    print(myo_1.bt.get_connections())
-    print(myo_2.bt.get_connections())
-
-    # Todo: check what the output print when connected, and check the output when disconnected
-    print('device name: %s' % myo_1.read_attr(0x03).payload)
-    print('device name: %s' % myo_2.read_attr(0x03).payload)
-
-    # qsize is broken on Mac.
-    # However, rewriting the method in the OS built-in library,
-    # such that it return the deque buffer length solve the issue.
-    # But is subject to report old data.
-
-    #def qsize(self):
-    #    # Raises NotImplementedError on Mac OSX because of broken sem_getvalue()
-    #    # return self._maxsize - self._sem._semlock._get_value()
-    #    return len(self._buffer) # Small Bug fix
-
-    def add_to_queue_myo1(emg, movement):
-        q1.put(emg)
-        while q1.qsize > BUFFER_SIZE:
-            q1.get()
-
-    def add_to_queue_myo2(emg, movement):
-        q2.put(emg)
-        while q2.qsize > BUFFER_SIZE:
-            q2.get()
-
-    myo_1.add_emg_handler(add_to_queue_myo1)
-    myo_2.add_emg_handler(add_to_queue_myo2)
-
-    # Vibrate to know we connected okay
-    # myo_1.vibrate(1)
-    # myo_2.vibrate(1)
-
     try:
-        while True:
-            myo_1.run()
-            myo_2.run()
+        # To change the mode of data, edit mode=emg_mode.<YourMode>
+        myo_1 = Myo(mode=emg_mode.RAW)
+        myo_2 = Myo(mode=emg_mode.RAW)
 
-    except KeyboardInterrupt:
-        myo_1.disconnect()
-        myo_2.disconnect()
+        myo_1.connect(ADDRESS_MYO1)  # RED LED
+        myo_2.connect(ADDRESS_MYO2)  # Turquoise LED
+
+        # Todo: check what the output print when connected, and check the output when disconnected
+        print(myo_1.bt.get_connections())
+        print(myo_2.bt.get_connections())
+
+        # Todo: check what the output print when connected, and check the output when disconnected
+        print('device name: %s' % myo_1.read_attr(0x03).payload)
+        print('device name: %s' % myo_2.read_attr(0x03).payload)
+
+        # qsize is broken on Mac.
+        # However, rewriting the method in the OS built-in library,
+        # such that it return the deque buffer length solve the issue.
+        # But is subject to report old data.
+
+        #def qsize(self):
+        #    # Raises NotImplementedError on Mac OSX because of broken sem_getvalue()
+        #    # return self._maxsize - self._sem._semlock._get_value()
+        #    return len(self._buffer) # Small Bug fix
+
+        def add_to_queue_myo1(emg, movement):
+            q1.put(emg)
+            while q1.qsize > BUFFER_SIZE:
+                q1.get()
+
+        def add_to_queue_myo2(emg, movement):
+            q2.put(emg)
+            while q2.qsize > BUFFER_SIZE:
+                q2.get()
+
+        myo_1.add_emg_handler(add_to_queue_myo1)
+        myo_2.add_emg_handler(add_to_queue_myo2)
+
+        # Vibrate to know we connected okay
+        # myo_1.vibrate(1)
+        # myo_2.vibrate(1)
+
+        try:
+            while True:
+                myo_1.run()
+                myo_2.run()
+
+        except KeyboardInterrupt:
+            myo_1.disconnect()
+            myo_2.disconnect()
+
+    except ValueError as ve:
+        _RED_ = '\033[91m'
+        _END_ = '\033[0m'
+        print(_RED_ + str(ve) + _END_)
 
 
 if __name__ == "__main__":
